a0ee4141f0c2877c11b5f42122d9156da6c2b61e
MyronFernandes <96162127+MyronFernandes@users.noreply.github.com>
2024-03-20 22:25:32 +0530

    Update AStar8puzzleproblem.py

--- AStar8puzzleproblem.py
+++ AStar8puzzleproblem.py
@@ -1,10 +1,10 @@
 import heapq
 import copy
+closed_set=[]
+print("Start State=")
+start_state =  [[int(x) for x in input(f"Enter the {i+1} row of the start matrix=  ").split(" ",3)] for i in range(3)]
 print("Goal State=")
 goal_state =[[int(x) for x in input(f"Enter the {i+1} row of the Goal matrix=  ").split(" ",3)] for i in range(3)]
-'''[[1,2,3],
-             [8,0,4],
-             [7,6,5]]'''
 def heuristic(state,gn):
     distance = 0
     for i in range(3):
@@ -12,30 +12,37 @@ def heuristic(state,gn):
             if (state[i][j] != goal_state[i][j])and(state[i][j]!=0):
                  distance=distance+1
     return distance+gn
+def h(state):
+    distance = 0
+    for i in range(3):
+        for j in range(3):
+            if (state[i][j] != goal_state[i][j])and(state[i][j]!=0):
+                 distance=distance+1
+    return distance
 def astar(start_state):
     open_list = []
-    closed_set = set()
+    closed_set.append((start_state,0,"Stable"))
     gn=0
     heapq.heapify(open_list)
     heapq.heappush(open_list, (heuristic(start_state,gn), (start_state,"Stable")))
     while open_list:
         current_cost, temp= heapq.heappop(open_list)
         current_state ,direct=temp
-        pre_dir=direct
-        closed_set.add(tuple(map(tuple, current_state)))
-        for next_state,direction in generate_next_states(current_state,pre_dir):
+        gn=current_cost-h(current_state)
+        closed_set.append((current_state,gn,direct))
+        gn=gn+1
+        for next_state,direction in generate_next_states(current_state,direct):
             print("current_cost F(n)=G(n)+h(n)=",heuristic(next_state,gn),",G(n)=",gn,"h(n)=",heuristic(next_state,gn)-gn,"          ",direction)
             for row in next_state:
                 print(row)
             if next_state == goal_state:
                 print("Goal State Found:")
-                print("direction=  H(n):",",direction =",direction )
+                print("direction=  H(n):",heuristic(next_state,gn),",G(n)=",gn,",direction =",direction )
                 for row in next_state:
                     print(row)
-                return current_state  # Return goal state if found
+                return next_state,direction,gn  # Return goal state if found
             if tuple(map(tuple, next_state)) not in closed_set:
                 heapq.heappush(open_list, (heuristic(next_state,gn),( next_state,direction)))
-        gn=gn+1
 def generate_next_states(state,pre_dir):
     next_states = []
     zero_x, zero_y = find_zero_position(state)
@@ -58,15 +65,56 @@ def generate_next_states(state,pre_dir):
             new_state[new_x][new_y] = 0
             next_states.append((new_state, direction))
     return next_states
+def previous_states_path(state,dir):
+    zero_x, zero_y = find_zero_position(state)
+    if dir == "Right":
+        new_x, new_y = 0 + zero_x,  zero_y-1 
+        if 0 <= new_x < 3 and 0 <= new_y < 3:
+            new_state = copy.deepcopy(state)
+            new_state[zero_x][zero_y] = new_state[new_x][new_y]
+            new_state[new_x][new_y] = 0
+            return new_state
+    elif dir == "Left":
+        new_x, new_y = 0 + zero_x, 1 + zero_y
+        if 0 <= new_x < 3 and 0 <= new_y < 3:
+            new_state = copy.deepcopy(state)
+            new_state[zero_x][zero_y] = new_state[new_x][new_y]
+            new_state[new_x][new_y] = 0
+            return new_state
+    elif dir == "Down":
+        new_x, new_y =  zero_x-1 , 0 + zero_y
+        if 0 <= new_x < 3 and 0 <= new_y < 3:
+            new_state = copy.deepcopy(state)
+            new_state[zero_x][zero_y] = new_state[new_x][new_y]
+            new_state[new_x][new_y] = 0
+            return new_state
+    elif dir == "Up":
+        new_x, new_y = 1 + zero_x, 0 + zero_y
+        if 0 <= new_x < 3 and 0 <= new_y < 3:
+            new_state = copy.deepcopy(state)
+            new_state[zero_x][zero_y] = new_state[new_x][new_y]
+            new_state[new_x][new_y] = 0
+            return new_state
+    return state
 def find_zero_position(state):
     for i in range(3):
         for j in range(3):
             if state[i][j] == 0:
                 return i, j
-print("Start State=")
-start_state =  [[int(x) for x in input(f"Enter the {i+1} row of the start matrix=  ").split(" ",3)] for i in range(3)]
-'''
-[[2,8,3],
-                [1,6,4],
-                [7,0,5]]#'''
-astar(start_state)
+state,direction,gn=astar(start_state)
+closed_set.append((state,gn,direction))
+print("Reverse path from goal to intial node=")
+print(f"GN({gn})for the following state are=")
+for row in state:
+    print(row)
+while gn!=0:
+    gn=gn-1
+    state=previous_states_path(state,direction)
+    tuple_to_search=(state,gn)
+    for item in closed_set:
+        if item[:2] == tuple_to_search:
+            direction = item[2]
+            break
+    print(f"GN({gn})for the following state are=")
+    for row in state:
+        print(row)
